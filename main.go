db6342d63c6f8906e438199b03824cf986437dd6
Alexander Bykov <113583500+AlexanderGhosty@users.noreply.github.com>
2025-02-15 22:47:26 +0300

    add loginHandler

--- main.go
+++ main.go
@@ -6,9 +6,75 @@ import (
     "log"
     "net/http"
     "encoding/json"
+    "time"
+    "context"
+
+	// Auntification
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/lib/pq"
 )
+
+var jwtKey = []byte("SUPER_SECRET_KEY")
+
+type Claims struct {
+    UserID int    `json:"user_id"`
+    Email  string `json:"email"`
+    jwt.RegisteredClaims
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Error(w, "Метод не поддерживается", http.StatusMethodNotAllowed)
+        return
+    }
+
+    var creds struct {
+        Email    string `json:"email"`
+        Password string `json:"password"`
+    }
+
+    if err := json.NewDecoder(r.Body).Decode(&creds); err != nil {
+        http.Error(w, "Некорректные данные", http.StatusBadRequest)
+        return
+    }
+
+    var user User
+    var hashedPassword string
+    err := db.QueryRow("SELECT id, name, email, password FROM users WHERE email = $1", creds.Email).
+        Scan(&user.ID, &user.Name, &user.Email, &hashedPassword)
+    if err != nil {
+        http.Error(w, "Неверный логин или пароль", http.StatusUnauthorized)
+        return
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(creds.Password)); err != nil {
+        http.Error(w, "Неверный логин или пароль", http.StatusUnauthorized)
+        return
+    }
+
+    expirationTime := time.Now().Add(60 * time.Minute)
+    claims := &Claims{
+        UserID: user.ID,
+        Email:  user.Email,
+        RegisteredClaims: jwt.RegisteredClaims{
+            ExpiresAt: jwt.NewNumericDate(expirationTime),
+        },
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    tokenString, err := token.SignedString(jwtKey)
+    if err != nil {
+        http.Error(w, "Ошибка при генерации токена", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+}
+
+
 
 // Var to store DB connection
 var db *sql.DB
